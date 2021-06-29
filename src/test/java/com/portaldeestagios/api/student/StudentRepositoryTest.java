55317ff5ad6ee99dfe8eb3fbae8b5827367d2485
Bruno Segato <brunocgato@hotmail.com>
2021-06-29 20:12:09 -0300

    Jpa test

--- src/test/java/com/portaldeestagios/api/student/StudentRepositoryTest.java
+++ src/test/java/com/portaldeestagios/api/student/StudentRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 @DataJpaTest
 @DisplayName("Student Repository test")
 class StudentRepositoryTest {
@@ -14,13 +16,42 @@ class StudentRepositoryTest {
     private StudentRepository studentRepository;
 
     @Test
-    @DisplayName("Test if student is create correct")
-    void save_PersistAnime_WhenSuccessful(){
+    @DisplayName("Test if student is persisted correct")
+    void save_PersistStudent_WhenSuccessful(){
 
         Student student = studentBuilder();
-        Student savedStudent = studentRepository.save(student);
+        Student savedStudent = this.studentRepository.save(student);
+
         Assertions.assertThat(savedStudent).isNotNull();
         Assertions.assertThat(savedStudent.getId()).isNotNull();
+        Assertions.assertThat(savedStudent.getFirstName().equals(student.getFirstName()));
+    }
+
+
+    @Test
+    @DisplayName("Test if student is updated correct")
+    void save_UpdatedStudent_WhenSuccessful(){
+
+        Student student = studentBuilder();
+        Student savedStudent = this.studentRepository.save(student);
+        savedStudent.setFirstName("Alexandre");
+        Student updatedStudent = this.studentRepository.save(student);
+
+        Assertions.assertThat(savedStudent).isNotNull();
+        Assertions.assertThat(savedStudent.getId()).isNotNull();
+        Assertions.assertThat(updatedStudent.getFirstName().equals(savedStudent.getFirstName()));
+    }
+
+    @Test
+    @DisplayName("Test if student is deleted correct")
+    void delete_PersistStudent_WhenSuccessful(){
+
+        Student student = studentBuilder();
+        Student savedStudent = this.studentRepository.save(student);
+        this.studentRepository.delete(savedStudent);
+        Optional<Student> studentOptional = this.studentRepository.findById(savedStudent.getId());
+
+        Assertions.assertThat(studentOptional.isEmpty()).isTrue();
     }
 
 
