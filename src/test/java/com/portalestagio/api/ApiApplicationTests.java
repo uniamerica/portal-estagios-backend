5815306574c74f095b5825a155e2ce30fcfdd6a9
Bruno <brunocgato@hotmail.com>
2021-08-24 20:56:49 -0300

    Test selection process

---
+++ src/test/java/com/portalestagio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalestagio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
