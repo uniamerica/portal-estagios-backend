737121ce6769fea600fa1f240bc1303a43a11d84
BrunoSegato13 <brunocgato@hotmail.com>
2021-08-24 21:27:52 -0300

    Tests selectionProcessService

--- src/test/java/com/portaldeestagios/api/selectionprocess/SelectionProcessServiceTest.java
+++ src/test/java/com/portaldeestagios/api/selectionprocess/SelectionProcessServiceTest.java
@@ -6,17 +6,15 @@ import com.portaldeestagios.api.student.StudentRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SelectionProcessServiceTest {
