fa41641eeb3e127cb844da7607f62fd1e772c3c2
Vinicius Oliveira de Freitas <mac@macs-MacBook-Air.local>
2021-08-24 21:56:40 -0300

    Testes de falha do loadUserByUsername e findOrFail

--- src/test/java/com/portaldeestagios/api/user/ApplicationUserServiceTest.java
+++ src/test/java/com/portaldeestagios/api/user/ApplicationUserServiceTest.java
@@ -1,5 +1,8 @@
 package com.portaldeestagios.api.user;
 
+import com.portaldeestagios.api.exception.ResourceNotFoundException;
+import com.portaldeestagios.api.exception.SelectionProcessNotFoundException;
+import com.portaldeestagios.api.exception.UserNotFoundException;
 import com.portaldeestagios.api.student.Student;
 import com.portaldeestagios.api.student.StudentRepository;
 import org.junit.Before;
@@ -15,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,9 +27,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
 
 import static com.portaldeestagios.api.user.ApplicationUserRole.ROLE_ADMIN;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -81,5 +87,21 @@ public class ApplicationUserServiceTest {
         applicationUserService.findOrFail(1L);
 
         verify(applicationUserRepository, times(1)).findById(1L);
+    }
+    @Test
+    public void findOrFail_fail() throws UserNotFoundException {
+        ApplicationUser applicationUser = new ApplicationUser("vinicius.vof@outlook.com", "1234", ROLE_ADMIN);
+
+        Mockito.when(applicationUserRepository.findById(1L)).thenThrow(new UserNotFoundException(1L));
+
+        assertThrows(ResourceNotFoundException.class, () -> applicationUserService.findOrFail(1L));
+    }
+    @Test
+    public void loadUserByUsername_fail() throws UserNotFoundException {
+        ApplicationUser applicationUser = new ApplicationUser("vinicius.vof@outlook.com", "1234", ROLE_ADMIN);
+
+        Mockito.when(applicationUserRepository.findByEmail(applicationUser.getUsername())).thenThrow(new UsernameNotFoundException("Bad Credentials"));
+
+        assertThrows(AuthenticationException.class, () -> applicationUserService.loadUserByUsername(applicationUser.getUsername()));
     }
 }
