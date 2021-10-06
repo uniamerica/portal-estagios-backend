23fef3577dd6b33639bb73c52685253680b4e165
Milene Danelli <milene.danelli@friellaita.local>
2021-10-06 16:24:28 -0300

    início teste de integracao cadastro usuario.

---
+++ src/test/java/com/portaldeestagios/api/user/UserIT.java
@@ -0,0 +1,44 @@
+package com.portaldeestagios.api.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portaldeestagios.api.IntegrationTestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static com.portaldeestagios.api.user.ApplicationUserRole.ROLE_ADMIN;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@EnableAutoConfiguration
+public class UserIT extends IntegrationTestConfig {
+    @Autowired
+    private MockMvc mvc;
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void mustCreateUserAndReturnSuccess() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/application-user")
+                        .content(asJsonString(new ApplicationUser("mi@outlook.com", "1234", ROLE_ADMIN)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
+}
